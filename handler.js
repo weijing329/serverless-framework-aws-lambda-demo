261c4a366a50f169712e80b1ce3eeabd729587ed
王維靖 <weijing329@outlook.com>
2018-11-11 12:11:02 +0800

    update API

--- handler.js
+++ handler.js
@@ -1,5 +1,7 @@
 'use strict'
 
+const axios = require('axios')
+
 module.exports.hello = async (event, context) => {
   return {
     statusCode: 200,
@@ -13,12 +15,22 @@ module.exports.hello = async (event, context) => {
   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 }
 
-module.exports.getSourceIp = async (event, context) => {
+module.exports.getIpInfo = async (event, context) => {
+  const sourceIp = event.requestContext.identity.sourceIp
+  const ipstackApiKey = '98f360f6b242303826caee3958419e84'
+  const [{ data: ipstack }, { data: ipapi }] = await Promise.all([
+    axios.get(`http://api.ipstack.com/${sourceIp}
+?access_key=${ipstackApiKey}&connection=1&security=1`),
+    axios.get(`http://ip-api.com/json/${sourceIp}`),
+  ])
+
   return {
     headers: { 'Access-Control-Allow-Origin': '*' },
     statusCode: 200,
     body: JSON.stringify({
-      sourceIp: event.requestContext.identity.sourceIp,
+      sourceIp,
+      ipapi,
+      ipstack,
     }),
   }
 }
